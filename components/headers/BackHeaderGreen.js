4a2ba10a91327dc2be3a99514580c1dacf549a3d
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-29 13:41:37 -0500

    modified styling

--- components/headers/BackHeaderGreen.js
+++ components/headers/BackHeaderGreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native'
-import Icon from 'react-native-vector-icons/dist/FontAwesome'
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+ 
 
 const BackHeaderGreen = ({title}) => {
   return (
     <View style={styles.header}>
-        <Text style={styles.text}>{title}</Text>
-        <Icon name="arrow-left"/>
+        <View style={styles.container}>
+            <Icon style={styles.icon} name='arrow-left'/>
+            <Text style={styles.text}>{title}</Text>
+        </View>
     </View>
   )
 }
@@ -19,15 +22,28 @@ const styles = StyleSheet.create({
     header: {
         height: 120,
         backgroundColor: '#BFD8C6',
-        borderBottomRadius: 120/2,
         alignContent:'center',
         justifyContent: 'center',
     },
+    container:{
+        flexDirection: 'row', 
+        width: "100%",
+        alignContent: 'center', 
+        alignSelf: 'center', 
+        paddingTop: 60,
+
+    },
     text: {
         color: 'black',
-        fontSize: 20, 
-        textAlign: 'center',
+        fontSize: 24, 
+        paddingLeft: 20,
     },
+    icon: {
+        fontSize: 24,
+        paddingRight: 20,
+        paddingLeft: 20,
+        color: '#7D3F1B'
+    }
 })
 
 export default BackHeaderGreen
