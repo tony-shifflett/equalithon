9f15035399e4e6b1fa771656dfcb418a6ff285bb
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-29 13:43:26 -0500

    created BackHeaderWhite

---
+++ components/headers/BackHeaderWhite.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native'
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
+ 
+
+const BackHeaderWhite = ({title}) => {
+  return (
+    <View style={styles.header}>
+        <View style={styles.container}>
+            <Icon style={styles.icon} name='arrow-left'/>
+            <Text style={styles.text}>{title}</Text>
+        </View>
+    </View>
+  )
+}
+
+BackHeaderWhite.defaultProps = {
+    title: 'Okionu'
+}
+
+const styles = StyleSheet.create({
+    header: {
+        height: 120,
+        backgroundColor: 'white',
+        alignContent:'center',
+        justifyContent: 'center',
+    },
+    container:{
+        flexDirection: 'row', 
+        width: "100%",
+        alignContent: 'center', 
+        alignSelf: 'center', 
+        paddingTop: 60,
+
+    },
+    text: {
+        color: 'black',
+        fontSize: 24, 
+        paddingLeft: 20,
+    },
+    icon: {
+        fontSize: 24,
+        paddingRight: 20,
+        paddingLeft: 20,
+        color: '#7D3F1B'
+    }
+})
+
+export default BackHeaderWhite
