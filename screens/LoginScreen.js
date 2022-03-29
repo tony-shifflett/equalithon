1033819a2f883d22adc89e31ceee4c75703b248e
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-29 13:08:52 -0500

    modified button rendering

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -2,22 +2,25 @@ import React, {useState} from 'react';
 import { View } from 'react-native';
 import LogoHeader from '../components/headers/LogoHeader';
 import OptionsBar from '../components/OptionsBar';
-import LargeButton from '../components/buttons/LargeButton'
+import LargeButton from '../components/buttons/LargeButton';
+import SmallButton from '../components/buttons/SmallButton';
 import SignUpForm from '../components/forms/SignUpForm';
 import LoginForm from '../components/forms/LoginForm';
 
 const LoginScreen = () => {
-    const [form, setForm] = useState("")
+    const [form, setForm] = useState('login')
     const formStateChange = (formName)=>{
         setForm(formName)
         console.log(form)
     }
-
+    let buttonText
     const checkState = () =>{
         console.log('rerendering')
         if (form == 'signup'){
+            buttonText = 'Sign Up'
             return <SignUpForm/>
         } else if (form == 'login'){
+            buttonText = 'Log in'
             return <LoginForm/>
         }
     }
@@ -26,7 +29,7 @@ const LoginScreen = () => {
         <LogoHeader/>
         <OptionsBar formSelector = {formStateChange}/>
         {checkState()}
-        <LargeButton/>
+        {form == 'login' ? <LargeButton buttonText={buttonText}/> : <SmallButton buttonText={buttonText}/>}
     </View>
   )
 }
