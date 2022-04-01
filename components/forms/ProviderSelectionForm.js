2281e1817fa0d74b8edce9d26227a8e7ef77231b
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-31 19:10:04 -0500

    styling

--- components/forms/ProviderSelectionForm.js
+++ components/forms/ProviderSelectionForm.js
@@ -17,12 +17,12 @@ const ProviderSelectionForm = () => {
         <Pressable style={styles.specialistType}>
             <Image style={styles.img}
             source={require('../../assets/pelvic-floor-specialist.jpg')}/>
-            <Text style={styles.txt}>Pelvic Floor Therapist</Text>
+            <Text style={styles.txt}>Pelvic Floor</Text><Text style={styles.txt}>Therapist</Text>
         </Pressable>
         <Pressable style={styles.specialistType}>
             <Image style={styles.img}
             source={require('../../assets/doula.jpg')}/>
-            <Text style={styles.txt}>Postpartum Doula</Text>
+            <Text style={styles.txt}>Postpartum</Text><Text style={styles.txt}>Doula</Text>
         </Pressable>
         <Pressable style={styles.specialistType}>
             <Image style={styles.img}
@@ -32,12 +32,12 @@ const ProviderSelectionForm = () => {
         <Pressable style={styles.specialistType}>
             <Image style={styles.img}
             source={require('../../assets/cleaning-services.jpg')}/>
-            <Text style={styles.txt}>Home Cleaning</Text>
+            <Text style={styles.txt}>Home</Text><Text style={styles.txt}>Cleaning</Text>
         </Pressable>
         <Pressable style={styles.specialistType}>
             <Image style={styles.img}
             source={require('../../assets/lactation-specialist.jpg')}/>
-            <Text style={styles.txt}>Lactation Specialist</Text>
+            <Text style={styles.txt}>Lactation</Text><Text style={styles.txt}>Specialist</Text>
         </Pressable>
     </View>
   )
@@ -47,16 +47,23 @@ const styles = StyleSheet.create({
     form: {
         height:379, 
         width: 343,
+        flexDirection: 'row', 
+        flexWrap: 'wrap',
+        justifyContent: 'space-between',
     },
     specialistType: {
-        borderWidth: 1,
+        width: 95,
+        marginTop: 56,
     },
     img: {
-        height: 95,
-        width: 75,
+        height: 75,
+        width: 95,
+        borderRadius: 100/16
     },
     txt: {
         fontSize: 16,
+        alignSelf: 'center',
+        fontWeight: '600',
     },
 })
 export default ProviderSelectionForm
