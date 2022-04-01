b812d559d686c86b59993bba785d4f159a5421e4
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-04-01 00:22:55 -0500

    styling

--- components/forms/SettingSelection.js
+++ components/forms/SettingSelection.js
@@ -4,33 +4,69 @@ import Icon from 'react-native-vector-icons/dist/FontAwesome'
 
 const SettingSelection = () => {
   return (
-    <View>
-        <TouchableOpacity>
-            <Text>Notifications</Text>
-            <Icon/>
+    <View style={styles.form}>
+        <TouchableOpacity style={styles.item}>
+            <Text style={styles.txt}>Notifications</Text>
+            <Icon name='chevron-right'
+            style={styles.icon}/>
         </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Manage Subscription</Text>
-            <Icon/>
+        <TouchableOpacity style={styles.item}>
+            <Text style={styles.txt}>Manage Subscription</Text>
+            <Icon name='chevron-right'
+            style={styles.icon}/>
         </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Payment on file</Text>
-            <Icon/>
+        <TouchableOpacity style={styles.item}>
+            <Text style={styles.txt}>Payment on file</Text>
+            <Icon name='chevron-right'
+            style={styles.icon}/>
         </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Reset Password</Text>
-            <Icon/>
+        <TouchableOpacity style={styles.item}>
+            <Text style={styles.txt}>Reset Password</Text>
+            <Icon name='chevron-right'
+            style={styles.icon}/>
         </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Delete Account</Text>
-            <Icon/>
+        <TouchableOpacity style={styles.item}>
+            <Text style={styles.txt}>Delete Account</Text>
+            <Icon name='chevron-right'
+            style={styles.icon}/>
         </TouchableOpacity>
-        <TouchableOpacity>
-            <Text>Log out</Text>
-            <Icon/>
+        <TouchableOpacity style={styles.finalItem}>
+            <Text style={styles.txt}>Log out</Text>
+            <Icon name='chevron-right'
+            style={styles.icon}/>
         </TouchableOpacity>
     </View>
   )
 }
 
+const styles = StyleSheet.create({
+    form: {
+        marginTop: 20,
+    },
+    item:{
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        height: 70, 
+        borderBottomWidth: 1,
+        borderBottomColor: 'grey',
+        alignItems: 'center',
+    },
+    finalItem:{
+        flexDirection: 'row', 
+        justifyContent: 'space-between',
+        height: 70, 
+        alignItems: 'center',
+    },
+    txt:{
+        fontSize: 20, 
+        fontWeight: '600', 
+        paddingLeft: 20, 
+    }, 
+    icon: {
+        fontSize: 20, 
+        color: '#A6381F', 
+        paddingRight: 20,
+    }
+
+})
 export default SettingSelection
--- screens/AccountSettings.js
+++ screens/AccountSettings.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
+import SettingSelection from '../components/forms/SettingSelection'
 import BackHeaderGreen from '../components/headers/BackHeaderGreen'
 
 const AccountSettings = () => {
   return (
     <View>
         <BackHeaderGreen title={'Account Settings'}/>
+        <SettingSelection/>
     </View>
   )
 }
