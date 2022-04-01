132c92215f3d74e281f1a34c490f148111b8eea2
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-31 23:17:28 -0500

    created Acount Settings page

---
+++ screens/AccountSettings.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View } from 'react-native'
+import BackHeaderGreen from '../components/headers/BackHeaderGreen'
+
+const AccountSettings = () => {
+  return (
+    <View>
+        <BackHeaderGreen title={'Account Settings'}/>
+    </View>
+  )
+}
+
+export default AccountSettings
