33d99d6609ed19b865d33a8b4c6795e44bd37d2e
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-05-26 14:48:30 -0500

    added state for userType, abstracted pressable functionality to selectUserType()

--- screens/SelectService.js
+++ screens/SelectService.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import { View, Pressable, Image, Text, StyleSheet } from 'react-native'
-import Footer from '../components/Footer'
 import XHeaderGreen from '../components/headers/XHeaderGreen'
 import { useNavigation } from '@react-navigation/native'
+import { useState } from 'react'
 
 const SelectService = () => {
+
+    //state to store user type, for later usage with page availability, services, etc
+    const [userType, setUserType] = useState("")
+
+    const selectUserType =(newUserType, destination)=>{
+        setUserType(newUserType)
+        console.log(userType)
+        navigation.navigate(destination)
+    }
 
     const navigation = useNavigation();
     return (
         <View>
             <XHeaderGreen title="Who Are You?"/>
             <View>
-                <Pressable style={styles.container} onPress={()=>navigation.navigate('EditProfile')}>
+                <Pressable style={styles.container} onPress={()=>selectUserType("client", "EditProfile")}>
                     <Text style={styles.txt}>I Need Postpartum Services</Text>
                     <Image
                     source={require('../assets/i-need-services.jpg')}
                     style={styles.img}/>
                 </Pressable>
-                <Pressable style={styles.container} onPress={()=>navigation.navigate('BusinessIntake')}>
+
+                <Pressable style={styles.container} onPress={()=>selectUserType("provider", "BusinessIntake")}>
                     <Text style={styles.txt}>I Provide Postpartum Services</Text>
                     <Image
                     source={require('../assets/provider-services.jpg')}
